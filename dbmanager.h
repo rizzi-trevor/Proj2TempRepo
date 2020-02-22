288b67be19a4f01492d89832de02c3f72efda9f6
rizzi-trevor <59940203+rizzi-trevor@users.noreply.github.com>
2020-02-22 15:03:19 -0800

    Update dbmanager.h

--- dbmanager.h
+++ dbmanager.h
@@ -1,11 +1,27 @@
 #ifndef DBMANAGER_H
 #define DBMANAGER_H
 
+#include <QAbstractItemModel>
+#include <QtSql>
+#include <QDebug>
+#include <QFileInfo>
+#include "QSqlDatabase" //?
+#include "QSqlQuery"//?
+#include "QDebug"//?
 
 class DbManager
 {
+
+
 public:
-    DbManager();
+    DbManager(const QString &path);
+
+    void InitCollegeList(const QString &path);
+
+
+private:
+
+    QSqlDatabase myDB;
 };
 
 #endif // DBMANAGER_H
